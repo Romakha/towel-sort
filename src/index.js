c774335e07b0f43fead367ffb8900d6528577af2
matheria <sensenevercame@gmail.com>
2022-02-10 12:25:10 +0500

    feat: towel-sort passed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix = []) {
+  const result = [];
+  for (let i = 0; i < matrix.length; i += 1) {
+    for (let j = 0; j < matrix[i].length; j += 1) {
+      let index = i % 2 === 0 ? j : (matrix[i].length - j - 1);
+      result.push(matrix[i][index]);  
+    }
+  }
+  return result;
 }
